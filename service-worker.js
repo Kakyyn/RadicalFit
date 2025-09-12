931ac058a15f9fbaa0a768a9e38b6c083ee3ab32
Kakyyn <mau2794@gmail.com>
2025-09-12 17:29:31 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+  // Service worker installed
+});
+
+self.addEventListener('activate', event => {
+  // Service worker activated
+});
+
+self.addEventListener('fetch', event => {
+  // You can add caching logic here if needed
+});
